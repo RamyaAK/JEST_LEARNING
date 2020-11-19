e6bb7dd105465472f7faf64f45c3d982d89325bb
RamyaK <ext_rakulkarni@falabella.cl>
2020-11-19 23:29:55 +0530

    added 2 more tests

--- multiply.test.js
+++ multiply.test.js
@@ -1,6 +1,17 @@
 const multiply = require('./multiply');
 describe('test multiply positive scenario',()=>{
+    
     test('multiply 3 by 2 should return 6',()=>{
         expect(multiply(3,2)).toBe(6);
+    });
+
+    test('multiply negative scenario i.e multiply 5 by 2 should not be equal to 7',()=>{
+        expect(multiply(5,2)).not.toBe(7);
+    });
+    
+    test('Objects Comparision Test',()=>{
+       const data = {'first':"1"};
+       data['second']= "2";
+       expect(data).toEqual({"first":"1", "second":"2"});
     });
 });
