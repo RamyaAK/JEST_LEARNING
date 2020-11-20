2856e0b46f66045a935158025406e8c9682ea82a
RamyaK <ext_rakulkarni@falabella.cl>
2020-11-20 08:36:13 +0530

    Adding Truthiness Matchers

--- jestMatchersExpanded.test.js
+++ jestMatchersExpanded.test.js
@@ -17,3 +17,14 @@ test('multiply 3 by 2 should return 6',()=>{
     expect(multiply(3,2)).toBeLessThan(7);
     expect(multiply(3,2)).toBeLessThanOrEqual(6);
 });
+
+//Truthiness Matchers:
+
+test('null check',()=>{
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).toBeFalsy();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+})
